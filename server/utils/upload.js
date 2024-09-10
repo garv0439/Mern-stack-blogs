0ce6c186d8fd7d2bcd254e87508dbdb69b9fecc8
garv0439 <garvkadia@gmail.com>
2024-09-10 15:36:25 +0530

    major update in upload

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -3,6 +3,9 @@ import { GridFsStorage } from 'multer-gridfs-storage';
 import dotenv from 'dotenv';
 
 
+// create a connection to MongoDB
+
+
 dotenv.config();
 
 const username = process.env.DB_USERNAME;
@@ -12,15 +15,13 @@ const storage = new GridFsStorage({
     url: `mongodb://${username}:${password}@blogapp-shard-00-00.z6rbn.mongodb.net:27017,blogapp-shard-00-01.z6rbn.mongodb.net:27017,blogapp-shard-00-02.z6rbn.mongodb.net:27017/?ssl=true&replicaSet=atlas-g59wf1-shard-0&authSource=admin&retryWrites=true&w=majority&appName=BlogApp`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
-        const match = ["image/png", "image/jpg"];
-
-        // if(match.indexOf(file.mimitype) === -1) 
-        //     return `${Date.now()}-blog-${file.originalname}`;
-
-        return {
-            bucketName: "photos",
-            filename: `${Date.now()}-blog-${file.originalname}`
-        }
+         const filename = `${Date.now()}_${file.originalname}`;
+    
+    const fileInfo = {
+      filename: filename,
+      bucketName: 'images'
+    };
+    return fileInfo;
     }
 });
 
